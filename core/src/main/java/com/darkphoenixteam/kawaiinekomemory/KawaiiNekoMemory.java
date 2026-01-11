c92e2f2d1dc17deec5941422f5c5a5dc46024028
IgnisDarkphoenix <167255282+IgnisDarkphoenix@users.noreply.github.com>
2026-01-10 23:01:36 -0800

    Update KawaiiNekoMemory.java

--- core/src/main/java/com/darkphoenixteam/kawaiinekomemory/KawaiiNekoMemory.java
+++ core/src/main/java/com/darkphoenixteam/kawaiinekomemory/KawaiiNekoMemory.java
@@ -3,31 +3,16 @@ package com.darkphoenixteam.kawaiinekomemory;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.darkphoenixteam.kawaiinekomemory.ads.AdController;
 import com.darkphoenixteam.kawaiinekomemory.screens.SplashScreen;
 
 public class KawaiiNekoMemory extends Game {
     
     public SpriteBatch batch;
-    public AdController adController;
-    
-    // Constructor sin ads (para testing)
-    public KawaiiNekoMemory() {
-        this(null);
-    }
-    
-    // Constructor con ads
-    public KawaiiNekoMemory(AdController adController) {
-        this.adController = adController;
-    }
     
     @Override
     public void create() {
         Gdx.app.log("KawaiiNeko", "Game starting...");
-        
         batch = new SpriteBatch();
-        
-        // Iniciar con SplashScreen
         setScreen(new SplashScreen(this));
     }
     
@@ -39,11 +24,9 @@ public class KawaiiNekoMemory extends Game {
     @Override
     public void dispose() {
         Gdx.app.log("KawaiiNeko", "Game disposing...");
-        
         if (batch != null) {
             batch.dispose();
         }
-        
         if (screen != null) {
             screen.dispose();
         }

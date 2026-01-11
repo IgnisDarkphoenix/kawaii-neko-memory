01ccad2f0497dc627401afbe0030f48dbc7243a4
IgnisDarkphoenix <167255282+IgnisDarkphoenix@users.noreply.github.com>
2026-01-11 00:48:43 -0800

    Actualizar Main.java

--- core/src/main/java/com/darkphoenixteam/kawaiinekomemory/Main.java
+++ core/src/main/java/com/darkphoenixteam/kawaiinekomemory/Main.java
@@ -3,38 +3,16 @@ package com.darkphoenixteam.kawaiinekomemory;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Main extends ApplicationAdapter {
     
-    private SpriteBatch batch;
-    private BitmapFont font;
-    
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        font = new BitmapFont();
-        font.getData().setScale(2f);
-        
-        Gdx.app.log("KawaiiNeko", "Game initialized successfully!");
     }
     
     @Override
     public void render() {
         Gdx.gl.glClearColor(1f, 0.8f, 0.9f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        
-        batch.begin();
-        font.draw(batch, "Kawaii Neko Memory", 100, Gdx.graphics.getHeight() / 2f + 50);
-        font.draw(batch, "Build Success!", 100, Gdx.graphics.getHeight() / 2f);
-        font.draw(batch, "Touch to continue...", 100, Gdx.graphics.getHeight() / 2f - 50);
-        batch.end();
-    }
-    
-    @Override
-    public void dispose() {
-        if (batch != null) batch.dispose();
-        if (font != null) font.dispose();
     }
 }

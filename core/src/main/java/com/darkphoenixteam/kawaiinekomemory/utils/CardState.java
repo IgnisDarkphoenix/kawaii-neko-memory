97f80e5f72035ff87afa9647127a6cdafe5dd72e
IgnisDarkphoenix <167255282+IgnisDarkphoenix@users.noreply.github.com>
2026-01-11 00:47:37 -0800

    Delete core/src/main/java/com/darkphoenixteam/kawaiinekomemory/utils directory

--- core/src/main/java/com/darkphoenixteam/kawaiinekomemory/utils/CardState.java
+++
@@ -1,8 +0,0 @@
-package com.darkphoenixteam.kawaiinekomemory.utils;
-
-public enum CardState {
-    HIDDEN,      // Boca abajo
-    FLIPPING,    // Animación de volteo
-    REVEALED,    // Boca arriba (visible)
-    MATCHED      // Par encontrado (desaparece)
-}

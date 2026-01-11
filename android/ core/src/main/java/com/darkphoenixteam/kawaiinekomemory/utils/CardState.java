a0920d9dd77673de5d2c33712d8326f1e002fd73
IgnisDarkphoenix <167255282+IgnisDarkphoenix@users.noreply.github.com>
2026-01-10 22:14:51 -0800

    Crear  core/src/main/java/com/darkphoenixteam/kawaiinekomemory/utils/CardState.java

---
+++ android/ core/src/main/java/com/darkphoenixteam/kawaiinekomemory/utils/CardState.java
@@ -0,0 +1,8 @@
+package com.darkphoenixteam.kawaiinekomemory.utils;
+
+public enum CardState {
+    HIDDEN,      // Boca abajo
+    FLIPPING,    // Animación de volteo
+    REVEALED,    // Boca arriba (visible)
+    MATCHED      // Par encontrado (desaparece)
+}

314eb4cba9a382172a08e5a9c5b23435123d8779
IgnisDarkphoenix <167255282+IgnisDarkphoenix@users.noreply.github.com>
2026-01-10 22:15:38 -0800

    Crear core/src/main/java/com/darkphoenixteam/kawaiinekomemory/Main.java

---
+++ android/core/src/main/java/com/darkphoenixteam/kawaiinekomemory/Main.java
@@ -0,0 +1,14 @@
+package com.darkphoenixteam.kawaiinekomemory;
+
+import com.badlogic.gdx.Game;
+
+/**
+ * Punto de entrada principal.
+ * Redirige a KawaiiNekoMemory.
+ */
+public class Main extends KawaiiNekoMemory {
+    
+    public Main() {
+        super();
+    }
+}
